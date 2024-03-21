5db3dffc1b283a2cc29c0710789af492eb96f503
James Liu <jamesliu96@users.noreply.github.com>
2024-03-21 15:09:52 +0800

    build: upgrade deps

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
-import { Application, Router } from 'https://deno.land/x/railgun@v0.5.1/mod.ts';
-import { serveDir } from 'https://deno.land/std@0.211.0/http/file_server.ts';
-import { parseArgs } from 'https://deno.land/std@0.211.0/cli/parse_args.ts';
+import { Application, Router } from 'https://deno.land/x/railgun@v0.5.2/mod.ts';
+import { serveDir } from 'https://deno.land/std@0.220.1/http/file_server.ts';
+import { parseArgs } from 'https://deno.land/std@0.220.1/cli/parse_args.ts';
 import { Game } from './game.ts';
 import { User } from './user.ts';
 
